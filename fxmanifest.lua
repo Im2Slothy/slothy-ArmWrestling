3419a3b44282567b9cf3028489f49db69d98eb86
Chris "Slothy <69122728+Im2Slothy@users.noreply.github.com>
2025-03-08 19:18:07 -0600

    Update fxmanifest.lua

--- fxmanifest.lua
+++ fxmanifest.lua
@@ -3,7 +3,7 @@ game 'gta5'
 
 author 'Slothy#0'
 
-description 'Slothy Bus Stops'
+description 'Slothy Arm Wrestling - A fun arm wrestling minigame for FiveM'
 
 client_script 'client.lua'
 server_script 'server.lua'
